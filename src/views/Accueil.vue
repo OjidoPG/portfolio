5a27a684489a173b0b93cee8b9302d1767a24ddc
Pascal <giraud.pascal@outlook.com>
2019-09-18 23:35:16 +0200

    'accueil'

--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -3,11 +3,11 @@
     <v-navigation-drawer v-model="drawer" app>
       <v-row class="justify-center mt-5 mb-5">
         <v-img
-                src="@/assets/profil.jpg"
-                max-width="150"
-                max-height="200"
-                alt="Photo de profil"
-                position="center"
+          src="@/assets/profil.jpg"
+          max-width="150"
+          max-height="200"
+          alt="Photo de profil"
+          position="center"
         ></v-img>
       </v-row>
       <v-divider></v-divider>
@@ -20,38 +20,55 @@
     </v-app-bar>
 
     <v-content>
-      <v-container class="fill-height">
-        <v-row align="center" justify="center">
-        </v-row>
-
-      </v-container>
+      <div>
+        <p class="lead mb-5">
+          Pendant plus de dix ans, j’ai mis à profit mon expérience dans les relations humaines, dans la compréhension de l’autre et dans la résolution de situations critiques pour venir en aide à mes patients.
+          <br />Ah oui, j'ai oublié de vous dire, j'ai été infirmier !
+          <br />Cependant, quoique passionnant, ce métier ne permettait pas de mettre a profit ma créativité et ne permettait pas d’observer les résultats des actions menées.
+          <br />J’ai donc décidé de me réorienter pour me diriger vers une autre passion, l’informatique.
+          <br />Après une période de réorientation, j’ai obtenu le titre de "Concepteur Développeur Informatique" à l’AFPA en juin 2017. J’ai travaillé un an comme développeur informatique C# ASP.Net MVC 5 (razor).
+          <br />J’ai ensuite décidé de poursuivre ma montée en compétence en rejoignant le CESI pour une POEC RIL.
+          <br />J’ai depuis trouvé un emploi au sein de la société C2i santé.
+        </p>
+        <div>
+          <a href="https://www.linkedin.com/in/pascal-giraud-1a2513128" target="_blank">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://github.com/OjidoPG" target="_blank">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="https://www.facebook.com/pascal.giraud.1485537" target="_blank">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+      </div>
     </v-content>
     <v-footer color="indigo" app>
       <span class="white--text">&copy; 2019 - GIRAUD Pascal</span>
     </v-footer>
 
-      <cv :cv="cv"></cv>
+    <cv :cv="cv"></cv>
   </div>
 </template>
 
 <script>
-  import TreeView from '../components/TreeView';
-  import Cv from '../components/Cv';
+import TreeView from "../components/TreeView";
+import Cv from "../components/Cv";
 
 export default {
   name: "Accueil",
-  components:{
+  components: {
     TreeView,
-      Cv
+    Cv
   },
   props: {
     source: String
   },
   data: () => ({
     drawer: false,
-      cv: {
-          dialog: false
-      }
+    cv: {
+      dialog: false
+    }
   })
 };
 </script>
