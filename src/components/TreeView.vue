ed9514b9890253636899eb328c97c433b3836351
OjidoPG <giraud.pascal@outlook.com>
2019-09-18 14:17:17 +0200

    'poursuite'

--- src/components/TreeView.vue
+++ src/components/TreeView.vue
@@ -1,22 +1,32 @@
 <template>
-    <v-treeview
-            rounded
-            dense
-            hoverable
-            :items="items"
-    >
-        <template slot="label" slot-scope="{ item }">
-            <i v-bind:class="{string:item.icon}">&nbsp;&nbsp;&nbsp;</i>
-            <a @click="openDialog(item)">{{ item.name }}</a>
-        </template>
-    </v-treeview>
+    <div>
+        <v-treeview
+                rounded
+                dense
+                hoverable
+                :items="items"
+        >
+            <template slot="label" slot-scope="{ item }">
+                <i :class=item.icon>&nbsp;&nbsp;&nbsp;</i>
+                <a @click="openDialog(item)">{{ item.name }}</a>
+            </template>
+        </v-treeview>
+    </div>
 </template>
 
 <script>
     export default {
-        methods:{
-            openDialog(item){
-                alert(item.id);
+        props: ['cv'],
+        methods: {
+            openDialog(item) {
+                switch (item.id) {
+                    case 1 :
+                        this.cv.dialog = true;
+                        break;
+                    case 3:
+                        alert('Baccalauréat');
+                        break;
+                }
             }
         },
         data: () => ({
@@ -24,30 +34,59 @@
                 {
                     id: 1,
                     name: 'Curriculum vitae',
-                    icon:'far fa-file',
-                    children:null
+                    icon: 'far fa-file'
                 },
                 {
                     id: 2,
                     name: 'Diplômes :',
+                    icon: 'fas fa-graduation-cap',
                     children: [
                         {
                             id: 3,
                             name: 'Baccalauréat'
                         },
                         {
-                            id:4,
-                            name:'Développeur'
+                            id: 4,
+                            name: 'Développeur'
                         },
                         {
-                            id:5,
-                            name:'Infirmier'
+                            id: 5,
+                            name: 'Infirmier'
                         },
                         {
-                            id:6,
-                            name:'Secouriste'
+                            id: 6,
+                            name: 'Secouriste'
                         }
                     ],
+                },
+                {
+                    id: 7,
+                    name: 'Expériences :',
+                    icon: 'far fa-folder-open',
+                    children: [
+                        {
+                            id: 8,
+                            name: 'Infirmier'
+                        },
+                        {
+                            id: 9,
+                            name: 'Développeur'
+                        },
+                        {
+                            id: 10,
+                            name: 'Autres'
+                        }
+                    ],
+                },
+                {
+                    id: 11,
+                    name: 'Réalisations',
+                    icon: 'fas fa-keyboard'
+                },
+                {
+                    id: 12,
+                    name: 'Certifications',
+                    icon: 'fas fa-certificate'
                 },
             ],
         }),
--- src/views/Accueil.vue
+++ src/views/Accueil.vue
@@ -11,58 +11,7 @@
         ></v-img>
       </v-row>
       <v-divider></v-divider>
-      <tree-view></tree-view>
-
-
-
-
-        <v-list-item>
-
-        </v-list-item>
-        <v-list-item>
-
-        </v-list-item>
-        <v-list-item @click="goCv">
-          <v-list-item-action>
-            <i class="far fa-file"></i>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Curriculum vitae</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="goDiplomes">
-          <v-list-item-action>
-            <i class="fas fa-graduation-cap"></i>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Diplômes</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="goXp">
-          <v-list-item-action>
-            <i class="far fa-folder-open"></i>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Expériences</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="goReal">
-          <v-list-item-action>
-            <i class="fas fa-keyboard"></i>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Réalisations</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-list-item @click="goCert">
-          <v-list-item-action>
-            <i class="fas fa-certificate"></i>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Certifications</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
+      <tree-view :cv="cv"></tree-view>
     </v-navigation-drawer>
 
     <v-app-bar app color="indigo" dark>
@@ -80,39 +29,29 @@
     <v-footer color="indigo" app>
       <span class="white--text">&copy; 2019 - GIRAUD Pascal</span>
     </v-footer>
+
+      <cv :cv="cv"></cv>
   </div>
 </template>
 
 <script>
   import TreeView from '../components/TreeView';
+  import Cv from '../components/Cv';
 
 export default {
   name: "Accueil",
   components:{
-    TreeView
+    TreeView,
+      Cv
   },
   props: {
     source: String
   },
   data: () => ({
-    drawer: false
-  }),
-  methods: {
-    goCv() {
-      alert("goCv");
-    },
-    goDiplomes() {
-      alert("goDiplomes");
-    },
-    goXp() {
-      alert("goXp");
-    },
-    goReal() {
-      alert("goReal");
-    },
-    goCert() {
-      alert("goCert");
-    }
-  }
+    drawer: false,
+      cv: {
+          dialog: false
+      }
+  })
 };
 </script>
