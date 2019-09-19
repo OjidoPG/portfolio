d7141f7b46a8cc87585aef8bcb94ec3a82fefb4c
Pascal <giraud.pascal@outlook.com>
2019-09-19 21:17:52 +0200

    'pdf_cv_ok'

--- src/components/Cv.vue
+++ src/components/Cv.vue
@@ -1,41 +1,34 @@
 <template>
-    <v-row justify="center">
-        <v-dialog v-model="cv.dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-            <v-card>
-                <v-toolbar dark color="primary">
-                    <v-btn icon dark @click.stop.prevent.capture="cv.dialog = false">
-                        <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                    <v-toolbar-title>Settings</v-toolbar-title>
-                    <div class="flex-grow-1"></div>
-                </v-toolbar>
-                <v-img
-                src="@/assets/ocr.jpg"
-                max-width="150"
-                max-height="200"
-                alt="Photo de profil"
-                position="center"
-        ></v-img>
-            </v-card>
-        </v-dialog>
-    </v-row>
+  <v-row justify="center">
+    <v-dialog v-model="cv.dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+      <v-toolbar dark color="primary">
+        <v-btn icon dark @click.stop.prevent.capture="cv.dialog = false">
+          <v-icon>mdi-arrow-left-bold-circle</v-icon>
+        </v-btn>
+        <v-toolbar-title>Retour</v-toolbar-title>
+      </v-toolbar>
+      <object data="/CV.pdf" type="application/pdf"></object>
+    </v-dialog>
+  </v-row>
 </template>
 
 <script>
-    export default {
-        name: 'Cv',
-        data () {
-            return {
-                notifications: false,
-                sound: true,
-                widgets: false,
-            }
-        },
-        props:{
-            cv:{
-                type:Object,
-                required:true
-            }
-        }
+export default {
+  name: "Cv",
+  props: {
+    cv: {
+      type: Object,
+      required: true
     }
+  }
+};
 </script>
+
+<style>
+object {
+  height: 90%;
+  width: 90%;
+  margin: 0px;
+  padding: 0px;
+}
+</style>
